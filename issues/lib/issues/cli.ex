a625ed71699371d462d3b5ef66c7f720df8b0cdd
schiughi <schiughi@gmail.com>
2018-01-09 00:11:11 +0900

    create format logic

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -1,4 +1,5 @@
 defmodule Issues.CLI do
+  import Issues.TableFormatter, only: [ print_table_for_columns: 2 ]
 
   @default_count 4
 
@@ -44,11 +45,13 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({ user, project, _count }) do
+  def process({ user, project, count }) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response
     |> convert_to_list_of_maps
     |> sort_into_ascending_order
+    |> Enum.take(count)
+    |> print_table_for_columns([ "number", "created_at", "title" ])
   end
 
   def decode_response({ :ok, body }), do: body
