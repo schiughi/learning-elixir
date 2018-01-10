c4010743e1c9dc7b21108453a1c9906801c7dcd8
schiughi <schiughi@gmail.com>
2018-01-10 22:35:44 +0900

    fix argument

--- issues/lib/issues/github_issues.ex
+++ issues/lib/issues/github_issues.ex
@@ -17,7 +17,7 @@ defmodule Issues.GithubIssues do
     { :ok,  Poison.Parser.parse!(body) }
   end
 
-  def handle_response( { _, %{ status_code: ___, body: body } }) do
+  def handle_response( { _, %{ status_code: status, body: body } }) do
     Logger.error "Error #{status} returned"
     { :error, Poison.Parser.parse!(body) }
   end
